4213852e3edc622a0b14a0a7cc1b9c7708f7d9c0
Silverz34 <pradogordillomichael@gmail.com>
2026-04-18 17:08:26 -0600

    restriccion de ocurrencia, evento carrera para citas e insercion de cita

--- sp_agendar_cita.sql
+++ sp_agendar_cita.sql
@@ -18,15 +18,41 @@ BEGIN
     END IF;
 
     -- Verificar que el veterinario (exista, estado y bloqueo de ocurrencias)
-    SELECT activo, dias_descanso INTO v_vet_
+    SELECT activo, dias_descanso INTO v_vet_activo, v_dias_descanso
+    FROM veterinarios 
+    WHERE id = p_veterinario_id FOR UPDATE;
 
-
-    IF NOT v_vet_activo OR v_vet_activo IS NULL THEN
+    IF NOT FOUND THEN 
         RAISE EXCEPTION 'El veterinario con ID % no existe', p_veterinario_id;
+    END IF;
+    -- Verificar que el veterinario esté activo
+    IF v_vet_activo IS NULL OR v_vet_activo = FALSE THEN 
+        RAISE EXCEPTION 'El veterinario no se  encuentra activo';
+    END IF;
+
+    --validacion de dia de descanso
+    v_dia_semana := trim(lower(to_char(p_fecha_hora, 'tmday')));
+    IF  position(v_dia_semana in lower(v_dias_descanso)) > 0 THEN
+        RAISE EXCEPTION 'El veterinario no atiende los días %', v_dia_semana;
+    END IF;
+
+    --COLISION DE CITAS 
+    SELECT EXISTS(
+        SELECT 1 FROM citas 
+        WHERE veterinario_id = p_veterinario_id 
+        AND fecha_hora = p_fecha_hora
+        AND estado != 'CANCELADA'
+    ) INTO v_colision_existe;
+
+    IF v_colision_existe THEN
+        RAISE EXCEPTION 'Ya existe una cita programada para el veterinario en esa fecha y hora';
     END IF;
 
     -- Insertar la cita en la tabla de citas
-    INSERT INTO citas (mascota_id, veterinario_id, fecha_hora, motivo)
-    VALUES (p_mascota_id, p_veterinario_id, p_fecha_hora, p_motivo)
+    INSERT INTO citas (mascota_id, veterinario_id, fecha_hora, motivo, estado )
+    VALUES (p_mascota_id, p_veterinario_id, p_fecha_hora, p_motivo, 'AGENDADA')
     RETURNING id INTO p_cita_id;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Error al agendar la cita: %', SQLERRM;
 END;
