1405d283a488dd33d2f8214e59e17924ca7e4cfa
Silverz34 <pradogordillomichael@gmail.com>
2026-04-18 01:55:14 -0600

    reajuste a view, se me olvido que era con CTE y debe incluir mascotas sin vacunas previas

--- view_vacunacion_pend.sql
+++ view_vacunacion_pend.sql
@@ -2,16 +2,28 @@
 --View para mostrar las vacunaciones pendientes de cada mascota
 
 CREATE OR REPLACE VIEWv_ascotas_vacunacion_pendiente AS
+--olvide el CTE 
+WITH Ultima_vacuna AS (
+    SELECT
+        m.id AS mascota_id,
+        MAX(v.fecha_aplicacion) AS ultima_vacuna
+    FROM vacunas_aplicadas
+    GROUP BY mascota_id
+)
 SELECT
-    m.id AS mascota_id,
-    m.nombre AS mascota_nombre,
+    m.nombre AS nombre_mascota,
     m.especie, 
     d.nombre AS nombre_dueno,
     d.telefono AS telefono_dueno,
-    MAX(v.fecha_aplicacion) AS ultima_vacuna,
-    CURRENT_DATE - MAX(v.fecha_aplicacion) AS dias_desde_ultima_vacuna
+    v.ultima_vacuna
+    CURRENT_DATE - v.ultima_vacuna AS dias_desde_ultima_vacuna
+CASE 
+    WHEN uv.fecha_ultima_vacuna IS NULL THEN 'SIN VACUNAS'
+    ELSE 'VENCIDA'
+END AS estado_vacunacion 
 FROM mascotas m
 JOIN duenos d ON m.dueno_id = d.id
-GROUP BY m.id, m.nombre, m.especie, d.nombre , d.telefono
-HAVING MAX(v.fecha_aplicacion) IS NULL 
-OR (CURRENT_DATE - MAX(va.fecha_aplicacion)) > 365;
+LEFT JOIN 
+    UltimaVacuna uv ON m.id = uv.mascota_id
+WHERE v.ultima_vacuna IS NULL 
+OR (CURRENT_DATE - v.ultima_vacuna) > 365; -- Consideramos que la vacuna es válida por un año (365 días)
