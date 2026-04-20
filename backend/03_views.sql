acf636c61c56964c27aef7e70f88181c36520575
Silverz34 <pradogordillomichael@gmail.com>
2026-04-19 21:47:57 -0600

    arreglos en view

--- backend/03_views.sql
+++ backend/03_views.sql
@@ -1,29 +1,30 @@
-
---View para mostrar las vacunaciones pendientes de cada mascota
-
-CREATE OR REPLACE VIEW v_ascotas_vacunacion_pendiente AS
---olvide el CTE 
-WITH Ultima_vacuna AS (
-    SELECT
-        m.id AS mascota_id,
-        MAX(v.fecha_aplicacion) AS ultima_vacuna
-    FROM vacunas_aplicadas
-    GROUP BY mascota_id
+CREATE OR REPLACE VIEW v_mascotas_vacunacion_pendiente AS
+WITH UltimaVacuna AS (
+    SELECT 
+        mascota_id,
+        MAX(fecha_aplicacion) AS fecha_ultima_vacuna
+    FROM 
+        vacunas_aplicadas
+    GROUP BY 
+        mascota_id
 )
-SELECT
+SELECT 
     m.nombre AS nombre_mascota,
-    m.especie, 
+    m.especie,
     d.nombre AS nombre_dueno,
     d.telefono AS telefono_dueno,
-    v.ultima_vacuna,
-    CURRENT_DATE - v.ultima_vacuna AS dias_desde_ultima_vacuna
-CASE 
-    WHEN uv.fecha_ultima_vacuna IS NULL THEN 'NUNCA_VACUNADA'
-    ELSE 'VENCIDA'
-END AS prioridad 
-FROM mascotas m
-JOIN duenos d ON m.dueno_id = d.id
+    uv.fecha_ultima_vacuna,
+    CURRENT_DATE - uv.fecha_ultima_vacuna AS dias_desde_ultima_vacuna,
+    CASE 
+        WHEN uv.fecha_ultima_vacuna IS NULL THEN 'NUNCA_VACUNADA'
+        ELSE 'VENCIDA'
+    END AS prioridad
+FROM 
+    mascotas m
+JOIN 
+    duenos d ON m.dueno_id = d.id
 LEFT JOIN 
-    Ultima_vacuna uv ON m.id = uv.mascota_id
-WHERE v.ultima_vacuna IS NULL 
-OR (CURRENT_DATE - v.ultima_vacuna) > 365; -- Consideramos que la vacuna es válida por un año (365 días)
+    UltimaVacuna uv ON m.id = uv.mascota_id
+WHERE 
+    uv.fecha_ultima_vacuna IS NULL 
+    OR (CURRENT_DATE - uv.fecha_ultima_vacuna) > 365;
