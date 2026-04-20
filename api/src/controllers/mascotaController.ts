a1c5b51a5c40d85d8e3e0159679f91fa9a258d2b
Silverz34 <pradogordillomichael@gmail.com>
2026-04-20 01:09:45 -0600

    middleware sencillo y mascota controller, prevencion de concatenar string en querys

---
+++ api/src/controllers/mascotaController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import {pool} from "../config/database";
+
+export const buscarMascotas = async(req: Request, res: Response): Promise<void> => {
+    const vetId = req.headers['x-vet-id'] as string;
+    const rol = req.headers['x-rol'] as string; 
+
+    const busqueda = req.query.busqueda as string || '';
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        await client.query('SET ROLE $1', [rol]);
+        if(vetId){
+            await client.query(`SELECT set_config('app.current_vet_id', $1, true)`, [vetId]);
+        }
+        const querySQL = `
+            SELECT id, nombre, especie, fecha_nacimiento, dueno_id 
+            FROM mascotas 
+            WHERE nombre ILIKE $1
+        `;
+        const result = await client.query(querySQL, [`%${busqueda}%`]);
+
+        await client.query('COMMIT');
+        res.status(200).json(result.rows);
+
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.error('Error al buscar mascotas:', error);
+        res.status(500).json({ error: 'Error al buscar mascotas.' });
+    } finally {
+        client.release();
+    }
+};
