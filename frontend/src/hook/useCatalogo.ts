c3931237b41d26820892410233e342310f336b5f
Silverz34 <pradogordillomichael@gmail.com>
2026-04-22 21:46:33 -0600

    hook para mostrar los datos basicos de los veterinarios

--- frontend/src/hook/useCatalogo.ts
+++ frontend/src/hook/useCatalogo.ts
@@ -1,23 +1,26 @@
 'use client';
+
 import { useState, useEffect } from 'react';
 import { useApiClient } from './useApi';
+import { MascotaCatalogo, VacunaCatalogo, VeterinarioCatalogo} from '@/interface/datosBase';
+
 
 export const useCatalogos = () => {
   const { apiGet } = useApiClient();
-
-  const [listaMascotas, setListaMascotas] = useState<any[]>([]);
-  const [listaVacunas, setListaVacunas] = useState<any[]>([]);
-  const [listaVeterinarios, setListaVeterinarios] = useState<any[]>([]);
-  const [cargandoCatalogos, setCargandoCatalogos] = useState(true);
+  const [listaMascotas, setListaMascotas] = useState<MascotaCatalogo[]>([]);
+  const [listaVacunas, setListaVacunas] = useState<VacunaCatalogo[]>([]);
+  const [listaVeterinarios, setListaVeterinarios] = useState<VeterinarioCatalogo[]>([]);
+  const [cargandoCatalogos, setCargandoCatalogos] = useState<boolean>(true);
 
   useEffect(() => {
     const cargarCatalogos = async () => {
       setCargandoCatalogos(true);
       try {
+      
         const [resMascotas, resVacunas, resVeterinarios] = await Promise.all([
-          apiGet<any[]>('/mascotas?q='),
-          apiGet<any[]>('/vacunas'), 
-          apiGet<any[]>('/veterinarios')
+          apiGet<MascotaCatalogo[]>('/mascotas?q='),
+          apiGet<VacunaCatalogo[]>('/vacunas/nombres'), 
+          apiGet<VeterinarioCatalogo[]>('/veterinarios/nombres')
         ]);
 
         setListaMascotas(resMascotas.data || []);
@@ -31,7 +34,6 @@ export const useCatalogos = () => {
     };
 
     cargarCatalogos();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return {
